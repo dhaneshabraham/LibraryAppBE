53e427e881a8456d0e7c16c9cf57c4dc9c21c7c1
dhaneshabraham <dhaneshabraham@gmail.com>
2022-06-30 14:45:52 +0530

    new

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/LibraryApp', (err) => {
-    if (!err)
-        console.log('LibraryApp MongoDB connection succeeded.');
-    else
-        console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
-});
+mongoose.connect('mongodb://localhost:27017/LibraryApp');
+// , (err) => {
+//     if (!err)
+//         console.log('LibraryApp MongoDB connection succeeded.');
+//     else
+//         console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+// });
 
 module.exports = mongoose;
