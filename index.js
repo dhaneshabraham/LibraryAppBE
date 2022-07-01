f6fa749c82ad1d8d0f0405ffa175ceda3c39984e
dhaneshabraham <dhaneshabraham@gmail.com>
2022-07-01 11:53:44 +0530

    make it better

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ var bookController = require('./controllers/bookController.js');
 
 app.use(bodyParser.json());
 // app.use(cors({ origin: 'http://localhost:4200' }));
-
+// app.use(cors({ origin: 'mongodb://localhost:27017/LibraryApp' }));
 app.listen(3000, () => console.log('Server started at port : 3000'));
 
-app.use('api/books', bookController);
+app.use('/api/books', bookController);
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname+'/dist/LibraryApp/index.html'));
